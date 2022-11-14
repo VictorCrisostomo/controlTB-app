f8ebd76470f1d0a2d3913f310856ac7966833000
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2022-11-14 19:06:51 -0300

    create page TVTB

--- src/pages/tvtb/index.js
+++ src/pages/tvtb/index.js
@@ -18,6 +18,102 @@ const Tvtb = () => {
           </div>
         </div>
         <button className="btnAudio">Audio descrição <BsFillCaretRightFill /></button>
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| Campanha Tuberculose 2020</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/S8jteT4rPds?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| Infográfico animado Tuberculose</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/0TgYOLknoa0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| Prevenção e Saúde - 24 de Março: Dia Mundial de Combate à Tuberculose</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/VEtiFXeQU6w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| Todos juntos contra a tuberculose</h3>
+          </div>
+          <div className="imgContent">
+            <iframe width="100%" height="190" src="https://www.youtube.com/embed/2y-UC8JLOb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| Tuberculose em pacientes com HIV</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/7UuLENh1EXM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| Nossas batalhas (como funciona nosso sistema imunológico?)</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/ahzcg6dy5MM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| 5 exercícios para fortalecer o pulmão</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/CEfBBZoJ9Rg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| Alimentação e Imunidade (alimentos que ajudam na imunidade)</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/oK0WPxeboWI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div className="Cards">
+        <div className="ContentCard">
+          <div className="textToptitleCard">
+            <h3>| 3 receitas gostosas para Aumentar a Imunidade naturalmente</h3>
+          </div>
+          <div className="imgContent">
+          <iframe width="100%" height="190" src="https://www.youtube.com/embed/RPeQhYuhrWY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
       </div>
 
       <div className="btnsPvNxt">
